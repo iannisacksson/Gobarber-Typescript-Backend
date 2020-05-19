b761641cbbacfbb023f1c694b8bc330e7b44fc65
Iann Isacksson <iisackssonoliveira@gmail.com>
2020-05-18 23:34:17 -0300

    feat: Email template

--- src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,11 +1,8 @@
-import IParseTemplateMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 import IMailTemplateProvider from '../models/IMailTemplateProvider';
 
 class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({
-    template,
-  }: IParseTemplateMailTemplateDTO): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'Mail content';
   }
 }
 
