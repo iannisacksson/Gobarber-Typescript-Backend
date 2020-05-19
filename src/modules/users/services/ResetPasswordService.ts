c8673f4fb38b3278bdcd1e2f0431f22f1f30b613
Iann Isacksson <iisackssonoliveira@gmail.com>
2020-05-18 21:01:10 -0300

    feat: Email submissions to reset passwords

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -20,7 +20,7 @@ class ResetPasswordService {
     @inject('UserTokensRepository')
     private userTokensRepository: IUserTokensRepository,
 
-    @inject('hashProvider')
+    @inject('HashProvider')
     private hashProvider: IHashProvider,
   ) {}
 
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+import IMailProvider from './MailProvider/models/IMailStorage';
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider(),
 );
