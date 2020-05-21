14daaa026283100a3ba72a7743a1bcb427f9e7b1
Iann Isacksson <iisackssonoliveira@gmail.com>
2020-05-21 20:33:53 -0300

    feat: List Provoders

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,40 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepostirory from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepostirory: FakeUsersRepostirory;
+let listProviders: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepostirory = new FakeUsersRepostirory();
+    listProviders = new ListProvidersService(fakeUsersRepostirory);
+  });
+
+  it('should be able to list the provider', async () => {
+    const user1 = await fakeUsersRepostirory.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const user2 = await fakeUsersRepostirory.create({
+      name: 'John Trê',
+      email: 'johntre@example.com',
+      password: '123456',
+    });
+
+    const loggedUser = await fakeUsersRepostirory.create({
+      name: 'John Qua',
+      email: 'johnqua@example.com',
+      password: '123456',
+    });
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toEqual([user1, user2]);
+  });
+});
