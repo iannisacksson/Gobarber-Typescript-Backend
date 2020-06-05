87531f932e872f89e86518970abbffd971e85be8
Iann <iann.isacksson@mangoaplicativos.com.br>
2020-06-05 12:36:16 -0300

    feat: Invalidating cache to register new user

--- src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
+++ src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -2,4 +2,5 @@ export default interface ICacheProvider {
   save(key: string, value: any): Promise<void>;
   recover<T>(key: string): Promise<T | null>;
   invalide(key: string): Promise<void>;
+  invalidePrefix(prefix: string): Promise<void>;
 }
